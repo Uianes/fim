39ac08e9d2501034922581801ed1eb5a0e7c8ad4
Uianes <biondouianes@gmail.com>
2024-01-01 22:40:30 -0300

    When a voice came thundering from above

--- index.php
+++ index.php
@@ -4,17 +4,28 @@ include("views/header.php");
 
 include("views/navbar.php");
 ?>
-
 <div class="h1 container-fluid d-flex justify-content-center align-items-center" style="height: 100vh;">
-    <p class="p-5">Saudações! <br>
-        Progluh apresenta uma abordagem diferente para aprender Programação!
-    </p>
-    <button>
-        
-    </button>
+    <img class="img-fluid" width="50%" src="assets/lu1.png" alt="Menina programadora negra em estilo robótico">
+    <div class="row">
+        <div class="col-12">
+            <p class="p-5">Saudações! Eu sou a Luh! <br>
+                <br>
+                Progluh apresenta uma abordagem diferente para aprender Programação!
+            </p>
+        </div>
+        <div class="text-center col-12 p-5">
+            <button class="btn btn-success btn-lg">Entrar na conta</button>
+            <br>
+            <br>
+            <button class="btn btn-success btn-lg">Criar uma conta</button>
+        </div>
+
+    </div>
 </div>
 
-<?php
 
+
+<?php
+include("views/footerpage.php");
 include("views/footer.php");
 ?>
---
+++ views/footerpage.php
@@ -0,0 +1,21 @@
+<footer class="bg-body-tertiary text-center text-lg-start">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+        <br>
+        <div class="row container-fluid d-flex justify-content-center align-items-center small">
+            <div class="col-4 col-md-1"><small class="text-muted">Python</small></div>
+            <div class="col-4 col-md-1"><small class="text-muted">C</small></div>
+            <div class="col-4 col-md-1"><small class="text-muted">Pascal</small></div>
+            <div class="col-4 col-md-1"><small class="text-muted">Portugol</small></div>
+            <div class="col-4 col-md-1"><small class="text-muted">C++</small></div>
+            <div class="col-4 col-md-1"><small class="text-muted">Java</small></div>
+            <div class="col-4 col-md-1"><small class="text-muted">Javascript</small></div>
+            <div class="col-4 col-md-1"><small class="text-muted">C#</small></div>
+            <div class="col-4 col-md-1"><small class="text-muted">VisualG</small></div>
+        </div>
+        <br>
+        ©
+        <?php echo "" . date("Y"); ?> - Uso livre - Desenvolvido por Uianes Luiz Rockenbach Biondo
+        <br>
+        <a class="text-body" href="https://github.com/Uianes">Source Code</a>
+    </div>
+</footer>
