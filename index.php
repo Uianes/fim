57be3195f072cd1f2fea6ab5cb704caf8634d7aa
Uianes <biondouianes@gmail.com>
2024-01-01 22:03:04 -0300

    And there was silence over the ocean

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
+session_start();
 include("views/header.php");
 
-echo "<h1>Hello, world!</h1>";
+include("views/navbar.php");
+?>
+
+<div class="h1 container-fluid d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <p class="p-5">Saudações! <br>
+        Progluh apresenta uma abordagem diferente para aprender Programação!
+    </p>
+    <button>
+        
+    </button>
+</div>
+
+<?php
 
 include("views/footer.php");
 ?>
